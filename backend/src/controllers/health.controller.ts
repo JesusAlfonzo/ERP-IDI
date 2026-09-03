e6a48ec74d4db60a5918dc22bbf95e7fcaf9564f
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-09-03 18:13:50 -0400

    feat: actualizar función getHealth para verificar el estado del sistema de manera asíncrona

--- backend/src/controllers/health.controller.ts
+++ backend/src/controllers/health.controller.ts
@@ -1,10 +1,17 @@
 import type { Request, Response, NextFunction } from 'express';
-import { checkSystemHealth } from '../services/health.service.js';
+import { checkSystemStatus } from '../services/health.service.js';
 
-export const getHealth = (req: Request, res: Response, next: NextFunction) => {
+export const getHealth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const systemStatus = checkSystemHealth();
-    res.status(200).json(systemStatus);
+    const systemStatus = await checkSystemStatus();
+
+    // Si la base de datos falló devolvemos 503 (Servicio degradado), si no, 200 (OK)
+    const statusCode = systemStatus.status === 'OK' ? 200 : 503;
+    res.status(statusCode).json(systemStatus);
   } catch (error) {
     next(error);
   }

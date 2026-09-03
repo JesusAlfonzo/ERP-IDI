721fe212005de93f87ae1a54dfc08fa33041bc03
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-09-03 19:27:52 -0400

    feat: agregar rutas para la gestión de productos, monedas, proveedores y órdenes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import healthRoutes from './routes/health.routes.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 import authRoutes from './routes/auth.routes.js';
+import productRoutes from './routes/product.routes.js';
+import currencyRoutes from './routes/currency.routes.js';
+import supplierRoutes from './routes/supplier.routes.js';
+import orderRoutes from './routes/order.routes.js';
 
 const app: express.Application = express();
 
@@ -11,6 +15,10 @@ app.use(express.json());
 // Rutas
 app.use('/api/health', healthRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/currencies', currencyRoutes);
+app.use('/api/suppliers', supplierRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Middleware de manejo de errores
 app.use(errorHandler);

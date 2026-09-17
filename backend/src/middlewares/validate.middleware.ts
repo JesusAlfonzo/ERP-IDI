c01d4f1087a1fc378632886720e008118b89e0f2
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-09-16 21:11:24 -0400

    feat: agregar middleware de validación para solicitudes usando Zod

---
+++ backend/src/middlewares/validate.middleware.ts
@@ -0,0 +1,17 @@
+import type { Request, Response, NextFunction } from 'express';
+import { type ZodType } from 'zod';
+
+export const validateRequest = (schema: ZodType) => {
+  return async (
+    req: Request,
+    _res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      req.body = await schema.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
